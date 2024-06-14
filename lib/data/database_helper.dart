97c72c53bf9eec6acfbfe596dda5c28c25d0436d
KumarL <kumarlopezpty@gmail.com>
2024-06-13 19:57:38 -0500

    categorias

--- lib/data/database_helper.dart
+++ lib/data/database_helper.dart
@@ -2,56 +2,68 @@ import 'package:sqflite/sqflite.dart';
 import 'package:path/path.dart';
 
 class DatabaseHelper {
-  static final _databaseName = "compras.db";
-  static final _databaseVersion = 1;
-  static final table = 'compras';
-
-  static final columnId = 'id';
-  static final columnMonto = 'monto';
-  static final columnCategoria = 'categoria';
-  static final columnFecha = 'fecha';
-
-  DatabaseHelper._privateConstructor();
-  static final DatabaseHelper instance = DatabaseHelper._privateConstructor();
+  static final DatabaseHelper instance = DatabaseHelper._init();
 
   static Database? _database;
 
+  DatabaseHelper._init();
+
   Future<Database> get database async {
     if (_database != null) return _database!;
-    _database = await _initDatabase();
+
+    _database = await _initDB('finanzas.db');
     return _database!;
   }
 
-  _initDatabase() async {
-    String path = join(await getDatabasesPath(), _databaseName);
-    return await openDatabase(path,
-        version: _databaseVersion, onCreate: _onCreate);
+  Future<Database> _initDB(String filePath) async {
+    final dbPath = await getDatabasesPath();
+    final path = join(dbPath, filePath);
+
+    return await openDatabase(path, version: 1, onCreate: _createDB);
   }
 
-  Future _onCreate(Database db, int version) async {
+  Future _createDB(Database db, int version) async {
     await db.execute('''
-          CREATE TABLE $table (
-            $columnId INTEGER PRIMARY KEY AUTOINCREMENT,
-            $columnMonto REAL NOT NULL,
-            $columnCategoria TEXT NOT NULL,
-            $columnFecha TEXT NOT NULL
-          )
-          ''');
+      CREATE TABLE compras(
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        monto REAL NOT NULL,
+        categoria TEXT NOT NULL,
+        fecha TEXT NOT NULL
+      )
+    ''');
+    await db.execute('''
+      CREATE TABLE categorias(
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        nombre TEXT NOT NULL
+      )''');
+
+    await db.insert('categorias', {'nombre': 'Comida'});
+
   }
 
-  Future<int> insertCompra(Map<String, dynamic> row) async {
-    Database db = await instance.database;
-    return await db.insert(table, row);
+  Future<void> insertCompra(Map<String, dynamic> row) async {
+    final db = await instance.database;
+    await db.insert('compras', row);
+  }
+
+  Future<void> insertCategoria(Map<String, dynamic> row) async {
+    final db = await instance.database;
+    await db.insert('categorias', row);
   }
 
   Future<List<Map<String, dynamic>>> fetchCompras() async {
-    Database db = await instance.database;
-    return await db.query(table);
+    final db = await instance.database;
+    return await db.query('compras');
   }
 
-  Future<int> updateCompra(Map<String, dynamic> row) async {
-    Database db = await instance.database;
-    int id = row[columnId];
-    return await db.update(table, row, where: '$columnId = ?', whereArgs: [id]);
+Future<List<String>> consultarDatos() async {
+  final db = await instance.database;
+  List<Map<String, dynamic>> result = await db.query('categorias', columns: ['nombre']);
+  return List.generate(result.length, (index) => result[index]['nombre'] as String);
+}
+
+  Future close() async {
+    final db = await instance.database;
+    db.close();
   }
 }
